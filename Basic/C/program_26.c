2b9633714196e0a624854488d09cbda0613cbd67
Juhi Dwivedi <91658716+Inconsequential-24@users.noreply.github.com>
2022-10-23 17:28:14 +0530

    old compilers
    made it easier to run on old compilers as well

--- Basic/C/program_26.c
+++ Basic/C/program_26.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
+#include<conio.h>
 int main(){int i,j,rows;
+           clrscr();
 printf("Enter number of rows:");
 scanf("%d",&rows);
 for (i=1;i<=rows;i++)
@@ -10,5 +12,6 @@ printf("*");
 }
 printf("\n");
 }
+           getch();
 return 0;
 }
